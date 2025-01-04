5325d4be0e30978b0add411688d43c12eb54659c
MahiSastry <sastry.mahi@gmail.com>
2025-01-04 08:19:56 +0530

    Changed home message to test CI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # double underscores
 
 @app.route('/')
 def home():
-    return "Hello from Docker Container!"
+    return "Different message!"  # This should fail our test
 
 @app.route('/predict', methods=['POST'])
 def predict():
